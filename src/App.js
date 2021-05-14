5b022c5d1d1a48ebf47daa56ddcc3597455ab6be
Rakibul Hasan <rakibulhasan745101@gmail.com>
2021-05-14 22:58:46 +0600

    'portfolio-website'

--- src/App.js
+++ src/App.js
@@ -8,17 +8,7 @@ import Project from "./components/Project/Project";
 import Resume from "./components/Resume/Resume";
 import Sidebar from "./components/Sidebar/Sidebar";
 
-// let firebaseConfig = {
-//   apiKey: "AIzaSyDQlKXe0IE9PfQ5_DdfeswlxP0PVgmgWMU",
-//   authDomain: "portfolio-website-850e4.firebaseapp.com",
-//   projectId: "portfolio-website-850e4",
-//   storageBucket: "portfolio-website-850e4.appspot.com",
-//   messagingSenderId: "204280528191",
-//   appId: "1:204280528191:web:c3ddc0a41973a73fd3a78a"
-// };
-
 function App() {
-  // firebase.initializeApp(firebaseConfig);
   return (
     <Switch>
       <section id="main-page">
